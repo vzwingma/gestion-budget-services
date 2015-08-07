83503b8e35f12271b3bca466ae94b7106769d24b
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-08-07 21:59:08 +0200

    Configuration BDD par JNDI

--- src/main/java/com/terrier/finances/gestion/data/mongodb/SpringMongoDBConfig.java
+++ src/main/java/com/terrier/finances/gestion/data/mongodb/SpringMongoDBConfig.java
@@ -3,18 +3,32 @@
  */
 package com.terrier.finances.gestion.data.mongodb;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NamingException;
+import javax.naming.RefAddr;
+import javax.naming.Reference;
+import javax.naming.spi.ObjectFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 /**
- * Configuration de connexion à la BDD
+ * Configuration de connexion à la BDD via JNDI
  * @author vzwingma
  *
  */
-@Service
-public class SpringMongoDBConfig {
+public class SpringMongoDBConfig implements ObjectFactory {
 
 
 	/**
@@ -22,94 +36,86 @@ public class SpringMongoDBConfig {
 	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringMongoDBConfig.class);
 	
-	private String ipServer;
-	private String portServer;
-	private String loginMongoDB;
-	private String pwdMongoDB;
-	private String nomMongoDB;
-	private static final int DEFAULT_PORT_MONGODB = 27017;
 
+	/* (non-Javadoc)
+	 * @see javax.naming.spi.ObjectFactory#getObjectInstance(java.lang.Object, javax.naming.Name, javax.naming.Context, java.util.Hashtable)
+	 */
+	@Override
+	public Object getObjectInstance(Object obj, Name name, Context nameCtx,
+			Hashtable<?, ?> environment) throws Exception {
+		validateProperty(obj, "Invalid JNDI object reference");
+
+	    String db = null;
+	    String host = null;
+	    String username = null;
+	    String password = null;
+	    int port = 27017;
+
+	    Reference ref = (Reference) obj;
+	    Enumeration<RefAddr> props = ref.getAll();
+	    while (props.hasMoreElements()) {
+	        RefAddr addr = (RefAddr) props.nextElement();
+	        String propName = addr.getType();
+	        String propValue = (String) addr.getContent();
+	        if (propName.equals("db")) {
+	            db = propValue;
+	        } else if (propName.equals("host")) {
+	            host = propValue;
+	        } else if (propName.equals("username")) {
+	            username = propValue;
+	        } else if (propName.equals("password")) {
+	            password = propValue;
+	        } else if (name.equals("port")) {
+	            try {
+	                port = Integer.parseInt(propValue);
+	            } catch (NumberFormatException e) {
+	                throw new NamingException("Invalid port value " + propValue);
+	            }
+	        }
+	    }
+	    return createMondoDBConfig(host, port, username, password, db);
+	  
+	}
+
+
+	/**
+	 * 
+	 * @param host de la BDD
+	 * @param port de la BDD
+	 * @param username login
+	 * @param password mot de passe
+	 * @param db nom de la BDD
+	 * @return Template de connexion BDD
+	 * @throws Exception
+	 */
+	private MongoTemplate createMondoDBConfig(String host, int port, String username, String password, String db) throws Exception{
+		  //create mongo template
+	    ServerAddress serveur = new ServerAddress(host, port);
+		LOGGER.info("[INIT] Configuration de la connexion vers MongDB : [{}]", serveur);
+		if(username != null && username.length() > 0  && password != null){
+			LOGGER.debug(" User : [{}]", username);
+			MongoCredential credential = MongoCredential.createMongoCRCredential(username, "admin", password.toCharArray());
+			return new MongoTemplate(new SimpleMongoDbFactory(new MongoClient(serveur, Arrays.asList(credential)), db));
+		}
+		else{
+			LOGGER.warn("[INIT] Configuration de dévelopement");
+			return new MongoTemplate(new SimpleMongoDbFactory(new MongoClient(serveur), db));
+		}
+	}
 	
-
-
+	
 	/**
-	 * @return the ipServer
+	 * Validate internal Object properties
+	 * 
+	 * @param property
+	 * @param errorMessage
+	 * @throws NamingException
 	 */
-	public String getIpServer() {
-		return ipServer;
-	}
-
-	/**
-	 * @param ipServer the ipServer to set
-	 */
-	@Value("${budget.data.ip}")
-	public void setIpServer(String ipServer) {
-		this.ipServer = ipServer;
-	}
-
-	/**
-	 * @return the portServer
-	 */
-	public int getPortServer() {
-		try{
-			return Integer.parseInt(this.portServer);
-		}
-		catch(NumberFormatException e){
-			LOGGER.warn("Le port configuré [{}] est incorrect. Utilisation de la valeur par défaut : [{}]", this.portServer, DEFAULT_PORT_MONGODB);
-		}
-		return DEFAULT_PORT_MONGODB;
-	}
-
-	/**
-	 * @param portServer the portServer to set
-	 */
-	@Value("${budget.data.port}")
-	public void setPortServer(String portServer) {
-		this.portServer = portServer;
-	}
-
-	/**
-	 * @return the loginMongoDB
-	 */
-	public String getLoginMongoDB() {
-		return loginMongoDB;
-	}
-
-	/**
-	 * @param loginMongoDB the loginMongoDB to set
-	 */
-	@Value("${budget.data.login}")
-	public void setLoginMongoDB(String loginMongoDB) {
-		this.loginMongoDB = loginMongoDB;
-	}
-
-	/**
-	 * @return the pwdMongoDB
-	 */
-	public String getPwdMongoDB() {
-		return pwdMongoDB;
-	}
-
-	/**
-	 * @param pwdMongoDB the pwdMongoDB to set
-	 */
-	@Value("${budget.data.password}")
-	public void setPwdMongoDB(String pwdMongoDB) {
-		this.pwdMongoDB = pwdMongoDB;
-	}
-
-	/**
-	 * @return the nomMongoDB
-	 */
-	public String getNomMongoDB() {
-		return nomMongoDB;
-	}
-
-	/**
-	 * @param nomMongoDB the nomMongoDB to set
-	 */
-	@Value("${budget.data.nom}")
-	public void setNomMongoDB(String nomMongoDB) {
-		this.nomMongoDB = nomMongoDB;
+	private void validateProperty(Object property, String errorMessage)
+	        throws NamingException {
+		LOGGER.info("validate property : {}", property);
+	    if (property == null) {
+	        throw new NamingException(errorMessage);
+	    }
 	}
 }
