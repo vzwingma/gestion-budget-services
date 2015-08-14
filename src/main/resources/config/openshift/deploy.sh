be884476451c505eff7081b59fcc3a98a05e0de5
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-08-14 13:38:45 +0200

    Ecrasement de la conf context.xml sur le fichier OpenShift

--- src/main/resources/config/openshift/deploy.sh
+++ src/main/resources/config/openshift/deploy.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 tar xvzf *.tar.gz
 mv $OPENSHIFT_DATA_DIR/ROOT.war $OPENSHIFT_JBOSSEWS_DIR/webapps/ROOT.war
-mv $OPENSHIFT_DATA_DIR/*.xml $OPENSHIFT_JBOSSEWS_DIR/conf
+cp $OPENSHIFT_DATA_DIR/context.prod.xml $OPENSHIFT_JBOSSEWS_DIR/conf
 mv *.tar.gz old/
