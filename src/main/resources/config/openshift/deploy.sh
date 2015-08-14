ee5fb2eae833e9fe60865bb52005bf14012f880f
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-08-14 14:24:17 +0200

    #33 ajout du script deploy dans le package

--- src/main/resources/config/openshift/deploy.sh
+++ src/main/resources/config/openshift/deploy.sh
@@ -1,5 +1,13 @@
 #/bin/bash
+#Verification du nombre d'arguments
+if [ $# -ne 1 ]        
+then                   
+    echo -e "\nErreur : Il manque le paramètre environnement {dev/prod}."
+    exit 1
+fi
+
+ENV = $1
 tar xvzf *.tar.gz
 mv $OPENSHIFT_DATA_DIR/ROOT.war $OPENSHIFT_JBOSSEWS_DIR/webapps/ROOT.war
-cp $OPENSHIFT_DATA_DIR/context.prod.xml $OPENSHIFT_JBOSSEWS_DIR/conf
+cp $OPENSHIFT_DATA_DIR/context.$ENV.xml $OPENSHIFT_JBOSSEWS_DIR/conf
 mv *.tar.gz old/
