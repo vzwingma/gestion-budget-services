1058796b2a68c845ee88056b7c6f73af85bd7de4
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2015-08-24 11:34:26 +0200

    ajout du listener de popupview

---
+++ src/main/java/com/terrier/finances/gestion/ui/listener/budget/mensuel/PopupNoteVisibitilityListener.java
@@ -0,0 +1,66 @@
+package com.terrier.finances.gestion.ui.listener.budget.mensuel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.terrier.finances.gestion.business.BusinessDepensesService;
+import com.terrier.finances.gestion.model.business.budget.BudgetMensuel;
+import com.terrier.finances.gestion.ui.sessions.UISessionManager;
+import com.vaadin.ui.PopupView.PopupVisibilityEvent;
+import com.vaadin.ui.PopupView.PopupVisibilityListener;
+import com.vaadin.ui.RichTextArea;
+
+/**
+ * Listener de visibilité de la popup de notes sur une dépense
+ * @author vzwingma
+ *
+ */
+public class PopupNoteVisibitilityListener implements PopupVisibilityListener {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8274118224737404597L;
+
+	/**
+	 * Logger
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(PopupNoteVisibitilityListener.class);
+
+	// Ligne de dépense
+	private final String idLigneDepense;
+	// Controleur
+	private final BusinessDepensesService serviceDepenses;
+	
+	/**
+	 * Popup visibility listener
+	 * @param ligneDepense
+	 * @param controleur
+	 */
+	public PopupNoteVisibitilityListener(final String idLigneDepense, final BusinessDepensesService serviceDepenses){
+		this.idLigneDepense = idLigneDepense;
+		this.serviceDepenses = serviceDepenses;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see com.vaadin.ui.PopupView.PopupVisibilityListener#popupVisibilityChange(com.vaadin.ui.PopupView.PopupVisibilityEvent)
+	 */
+	@Override
+	public void popupVisibilityChange(PopupVisibilityEvent event) {
+		if(!event.isPopupVisible()){
+			// Mise à jour de la ligne de dépense
+			RichTextArea rta = (RichTextArea)event.getPopupView().getContent().getPopupComponent();
+			LOGGER.debug("[IHM] Dépense [{}] Mise à jour de la note [{}]", idLigneDepense, rta.getValue());
+			this.serviceDepenses.majNotesLignesDepenses(
+					UISessionManager.getSession().getBudgetMensuelCourant(), 
+					idLigneDepense, 
+					rta.getValue(), 
+					UISessionManager.getSession().getUtilisateurCourant().getLogin());
+		}
+	}
+
+	
+	
+}
