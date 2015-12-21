33ea8f3b43d6ba0d37248dcbccbd255a11205bbb
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2015-12-21 10:57:31 +0100

    maj script de déploiement depuis GitHub

--- src/main/resources/config/openshift/deploy.sh
+++ src/main/resources/config/openshift/deploy.sh
@@ -1,19 +1,15 @@
 #/bin/bash
 #Verification du nombre d'arguments
-if [ $# -ne 2 ]        
+if [ $# -ne 1 ]
 then                   
     echo -e "\nErreur : Il manque des paramètres obligatoires"
 	echo -e "- la version applicative"
-	echo -e "- le numéro de build"
     exit 1
 fi
 
 export VERSION=$1
-export BUILD=$2
 
-wget https://s3-eu-west-1.amazonaws.com/gestion-budget/artifacts/$BUILD/target/Budget-v$VERSION.tar.gz
-
-
+wget https://github.com/vzwingma/gestion-budget/releases/download/$VERSION/Budget-v$VERSION.tar.gz
 tar xvzf *.tar.gz
 mv $OPENSHIFT_DATA_DIR/ROOT.war $OPENSHIFT_JBOSSEWS_DIR/webapps/ROOT.war
 rm *.tar.gz
