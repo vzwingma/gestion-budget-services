d6720b8ffc0b956f6146f567f172a1aff549a7bf
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2016-07-03 13:41:32 +0200

    Ajout du déploiement auto de la dernière version

--- src/main/resources/config/openshift/last-version-deploy.sh
+++ src/main/resources/config/openshift/last-version-deploy.sh
@@ -1,6 +1,22 @@
 #/bin/bash
+# Ce fichier doit être déposé sur OpenShift 
+# ici : ~/app-root/repo/.openshift/cron/minutely/last-version-deploy.sh
 PROJET_GITHUB=https://github.com/vzwingma/gestion-budget
 VERSION=`curl -Ls -o /dev/null -w %{url_effective} $PROJET_GITHUB/releases/latest/ |cut -d'/' -f8`
 
-echo "Déploiement de la version $VERSION"
-./deploy.sh $VERSION
+VERSION_PREC=`cat ~/app-root/data/.lastversion.file`
+
+LOG=~/app-root/data/cron.log
+echo "`date -u`" > $LOG;
+echo "Version précédente : $VERSION_PREC" >> $LOG;
+echo "Version GitHub     : $VERSION" >> $LOG;
+
+if [ "$VERSION_PREC" == "$VERSION" ]
+then
+        echo "Pas de nouvelle version détectée" >> $LOG;
+else
+        echo "Déploiement de la version $VERSION" >> $LOG;
+        cd ~/app-root/data
+        ./deploy.sh $VERSION >> $LOG;
+        echo $VERSION > .lastversion.file
+fi
