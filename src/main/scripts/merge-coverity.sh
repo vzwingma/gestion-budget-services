4cf90f4f303e6c82f0fcf16ab7f21240621cb3e8
vzwingma <vincent.zwingmann@gmail.com>
2016-12-25 13:49:50 +0100

    merge coverity

--- src/main/scripts/merge-coverity.sh
+++ src/main/scripts/merge-coverity.sh
@@ -3,6 +3,7 @@ echo ${TRAVIS_BRANCH}
 # Merge vers la branche coverity_scan
 if [ ${TRAVIS_BRANCH} = "snapshot" ]; then
 	echo "Merge vers la branche Coverity";
+	git branch coverity_scan
 	git checkout coverity_scan
 	git merge master
 	cp src/main/assembly/template/coverity.travis.yml .travis.yml
