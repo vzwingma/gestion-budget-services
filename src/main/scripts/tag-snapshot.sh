cf4574e37c7646e0bbc56252bf261df8d0744f31
vzwingma <vincent.zwingmann@gmail.com>
2016-12-25 11:11:12 +0100

    script du tag hors du travisCI

---
+++ src/main/scripts/tag-snapshot.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+echo ${TRAVIS_BRANCH}
+# Tag de snapshot
+if [ ${TRAVIS_BRANCH} = "snapshot" ]; then
+	echo "Pas de reTag de snapshot";
+else
+	echo "ReTag de snapshot";
+	git tag -d snapshot;
+	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget :refs/tags/snapshot;
+	git tag snapshot;
+	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget --tags;
+fi
