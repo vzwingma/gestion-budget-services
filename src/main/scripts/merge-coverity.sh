e47377f088c719bd53647e77174b5bd2d1225e84
vzwingma <vincent.zwingmann@gmail.com>
2016-12-25 12:03:10 +0100

    script du merge vers coverity

--- src/main/scripts/merge-coverity.sh
+++ src/main/scripts/merge-coverity.sh
@@ -3,6 +3,12 @@ echo ${TRAVIS_BRANCH}
 # Merge vers la branche coverity_scan
 if [ ${TRAVIS_BRANCH} = "snapshot" ]; then
 	echo "Merge vers la branche Coverity";
+	git checkout coverity_scan
+	git merge master
+	cp src/main/assembly/template/coverity.travis.yml .travis.yml
+	git add .travis.yml
+	git commit -m "Application du .travis.yml Coverity"
+	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget
 else
 	echo "Pas de merge vers la branche Coverity";
 fi
