acda07e641947b8bca734e74c34f0d671f0411f7
vzwingma <vincent.zwingmann@gmail.com>
2016-12-25 18:56:21 +0100

    tag de snapshot uniquement lors des push depuis Master

--- src/main/scripts/tag-snapshot.sh
+++ src/main/scripts/tag-snapshot.sh
@@ -2,12 +2,12 @@
 
 echo ${TRAVIS_BRANCH}
 # Tag de snapshot. C'est un tag pour permettre le déploiement sur GitHUB.
-if [ ${TRAVIS_BRANCH} = "snapshot" ]; then
-	echo "Pas de reTag de snapshot";
-else
+if [ ${TRAVIS_BRANCH} = "master" ]; then
 	echo "ReTag de snapshot";
 	git tag -d snapshot;
 	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget :refs/tags/snapshot;
 	git tag snapshot;
 	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget --tags;
+else
+	echo "Pas de reTag de snapshot";
 fi
