5829e62f7eb80c3661df9fa5008e6b19d1fd25c2
vzwingma <vincent.zwingmann@gmail.com>
2017-12-23 22:06:25 +0100

    suppression de JNDI : Usage direct des envs vars

--- src/main/java/com/terrier/finances/gestion/data/mongodb/SpringMongoDBConfig.java
+++ src/main/java/com/terrier/finances/gestion/data/mongodb/SpringMongoDBConfig.java
@@ -3,47 +3,30 @@
  */
 package com.terrier.finances.gestion.data.mongodb;
 
-import java.util.Hashtable;
-
-import javax.naming.Context;
-import javax.naming.Name;
-import javax.naming.spi.ObjectFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
 /**
- * Configuration de connexion à la BDD via JNDI
+ * Configuration de connexion à la BDD via des variables d'environnement
  * @author vzwingma
  *
  */
-public class SpringMongoDBConfig implements ObjectFactory {
+@Configuration
+public class SpringMongoDBConfig {
 
 
 	/**
 	 * Logger
 	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringMongoDBConfig.class);
-
-
-	/* (non-Javadoc)
-	 * @see javax.naming.spi.ObjectFactory#getObjectInstance(java.lang.Object, javax.naming.Name, javax.naming.Context, java.util.Hashtable)
-	 * 
-	 */
-	@Override
-	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
-		String db = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_DB);
-		String host = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_HOST);
-		String username = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_USERNAME);
-		String password = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_PWD);
-		int port =  getIntgerEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_PORT);
-		return createMondoDBConfig(host, port, username, password, db);
-
-	}
 
 
 	/**
@@ -56,7 +39,15 @@ public class SpringMongoDBConfig implements ObjectFactory {
 	 * @return Template de connexion BDD
 	 * @throws Exception
 	 */
-	private MongoTemplate createMondoDBConfig(String host, int port, String username, String password, String db) throws Exception{
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+
+		String db = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_DB);
+		String host = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_HOST);
+		String username = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_USERNAME);
+		String password = getStringEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_PWD);
+		int port =  getIntgerEnvVar(MongoDBCOnfigEnum.MONGODB_CONFIG_PORT);
+
 		//create mongo template
 		String mongoURI = new StringBuilder("mongodb://").append(username).append(":").append(password).append("@").append(host).append(":").append(port).append("/").append(db).toString();
 		String mongoURILog = new StringBuilder("mongodb://").append(username).append("@").append(host).append(":").append(port).append("/").append(db).toString();
@@ -74,7 +65,7 @@ public class SpringMongoDBConfig implements ObjectFactory {
 	private String getStringEnvVar(MongoDBCOnfigEnum cle){
 		return System.getenv(cle.name());
 	}
-	
+
 	/**
 	 * Retourne la valeur int de la variable d'environnement
 	 * @param cle
