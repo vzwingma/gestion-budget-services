ed1c43667b71a34ab9ec942f16cc9a8810446ef1
Vincent ZWINGMANN <vincent.zwingmann@soprasteria.com>
2018-01-05 18:14:33 +0100

    retour du script coverity

--- src/main/scripts/merge-coverity.sh
+++ src/main/scripts/merge-coverity.sh
@@ -5,10 +5,7 @@ if [ ${TRAVIS_BRANCH} = "snapshot" ]; then
 	echo "Merge vers la branche Coverity";
 	git branch coverity_scan
 	git checkout coverity_scan
-	git merge master
-	cp src/main/external-ressources/template/coverity.travis.yml .travis.yml
-	git add .travis.yml
-	git commit -m "Application du .travis.yml Coverity"
+	git rebase master
 	git config --global push.default simple
 	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget --set-upstream origin coverity_scan 
 else
