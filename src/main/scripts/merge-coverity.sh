d4786e88266af9d97c5fc23472911fe2e5ec295a
Vincent ZWINGMANN <vincent.zwingmann@soprasteria.com>
2018-01-09 13:18:10 +0100

    branch & checkout séparés

--- src/main/scripts/merge-coverity.sh
+++ src/main/scripts/merge-coverity.sh
@@ -3,8 +3,9 @@ echo ${TRAVIS_BRANCH}
 # Merge vers la branche coverity_scan
 if [ ${TRAVIS_BRANCH} = "master" ]; then
 	echo "Merge vers la branche Coverity";
-	git fetch origin
-	git checkout -b coverity_scan --track remotes/origin/coverity_scan
+	git fetch --all
+	git branch coverity_scan origin/coverity_scan 
+	git checkout coverity_scan
 	git rebase master
 	git config --global push.default simple
 	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget --set-upstream origin coverity_scan 
