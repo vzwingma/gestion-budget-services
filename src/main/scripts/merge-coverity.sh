018c49a63e3568ec0854765d5d4cd6292c03554c
Vincent ZWINGMANN <vincent.zwingmann@soprasteria.com>
2018-01-09 12:59:14 +0100

    avec remotes

--- src/main/scripts/merge-coverity.sh
+++ src/main/scripts/merge-coverity.sh
@@ -3,8 +3,8 @@ echo ${TRAVIS_BRANCH}
 # Merge vers la branche coverity_scan
 if [ ${TRAVIS_BRANCH} = "master" ]; then
 	echo "Merge vers la branche Coverity";
-	git fetch origin
-	git checkout -b coverity_scan origin/coverity_scan
+	git fetch --all && git fetch -t
+	git checkout -b coverity_scan remotes/origin/coverity_scan
 	git rebase master
 	git config --global push.default simple
 	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget --set-upstream origin coverity_scan 
