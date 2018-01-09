4106a55518ac7a6adae4ea15d6d4ad3ee358b1cf
Vincent ZWINGMANN <vincent.zwingmann@soprasteria.com>
2018-01-09 12:49:49 +0100

    nom de la branche ajoutée

--- src/main/scripts/merge-coverity.sh
+++ src/main/scripts/merge-coverity.sh
@@ -4,7 +4,7 @@ echo ${TRAVIS_BRANCH}
 if [ ${TRAVIS_BRANCH} = "master" ]; then
 	echo "Merge vers la branche Coverity";
 	git fetch origin
-	git checkout origin/coverity_scan
+	git checkout -b coverity_scan origin/coverity_scan
 	git rebase master
 	git config --global push.default simple
 	git push https://$GITHUB_API_KEY@github.com/vzwingma/gestion-budget --set-upstream origin coverity_scan 
