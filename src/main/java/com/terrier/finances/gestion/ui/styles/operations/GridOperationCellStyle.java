e3e2679675ce8e8bef60e1312326d78b1175e961
vzwingma <vincent.zwingmann@gmail.com>
2018-08-01 16:21:58 +0200

    Maj des styles sur la page

--- src/main/java/com/terrier/finances/gestion/ui/styles/operations/GridOperationCellStyle.java
+++ src/main/java/com/terrier/finances/gestion/ui/styles/operations/GridOperationCellStyle.java
@@ -16,30 +16,17 @@ public class GridOperationCellStyle implements StyleGenerator<LigneDepense> {
 
 	private static final long serialVersionUID = -6709397765771547573L;
 
-	private Boolean oddStyleLignes;
-
-	public GridOperationCellStyle(Boolean oddStyleLignes){
-		this.oddStyleLignes = oddStyleLignes;
-	}
-	public GridOperationCellStyle(){
-		this.oddStyleLignes = false;
-	}
 
 	@Override
 	public String apply(LigneDepense depense) {
 
 		// Style de la ligne
-		StringBuilder style = new StringBuilder("v-grid-cell-");
+		StringBuilder style = new StringBuilder("v-grid-row-");
 		//  Sauf pour les dépenses réalisées, et celle réserve
 
 		if(!OperationsService.ID_SS_CAT_RESERVE.equals(depense.getSsCategorie().getId())){
 			style.append(depense.getEtat().getId());
 		}
-
-		// Gestion du style par préférence utilisateur
-//		if(oddStyleLignes){
-//			style.append(rang%2 == 0 ? "" : "-odd");
-//		}
 
 		// Style de la dernière opération
 		if(depense.isDerniereOperation()){
