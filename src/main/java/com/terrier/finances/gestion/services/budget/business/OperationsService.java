2990a09adae83b4406314fee1d74554e68c9af5a
vzwingma <vincent.zwingmann@gmail.com>
2018-09-03 14:18:33 +0200

    #114 : Sélection du mode mensuel pour les prélèvements

--- src/main/java/com/terrier/finances/gestion/services/budget/business/OperationsService.java
+++ src/main/java/com/terrier/finances/gestion/services/budget/business/OperationsService.java
@@ -64,7 +64,9 @@ public class OperationsService extends AbstractBusinessService {
 	public static final String ID_SS_CAT_RESERVE = "26a4b966-ffdc-4cb7-8611-a5ba4b518ef5";
 	public static final String ID_SS_CAT_PREVISION_SANTE = "eeb2f9a5-49b4-4c44-86bf-3bd626412d8e";
 
-
+	public static final String ID_CAT_PRELEVEMENTS_MENSUEL = "504beea7-ed52-438a-aced-15e9603b82ab";
+	
+	
 	/**
 	 * Chargement du budget du mois courant
 	 * @param compte compte 
