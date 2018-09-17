189fffe1bd9bf468b6e2281f9cbe1ce8801b1581
ZWINGMANN Vincent <vincent.zwingmann@soprasteria.com>
2018-09-17 22:00:09 +0200

    suppression d'import

--- src/main/java/com/terrier/finances/gestion/services/utilisateurs/api/UtilisateursAPIController.java
+++ src/main/java/com/terrier/finances/gestion/services/utilisateurs/api/UtilisateursAPIController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
