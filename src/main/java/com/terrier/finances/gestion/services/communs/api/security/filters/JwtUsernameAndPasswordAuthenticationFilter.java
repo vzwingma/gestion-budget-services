3ae4deaff81e8c8ac52686a136accd0c4eeed8bb
ZWINGMANN Vincent <vincent.zwingmann@soprasteria.com>
2018-09-20 14:23:31 +0200

    suppression d'imports

--- src/main/java/com/terrier/finances/gestion/services/communs/api/security/filters/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/terrier/finances/gestion/services/communs/api/security/filters/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -21,7 +21,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
