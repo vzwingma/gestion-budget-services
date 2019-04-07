295557296949437a9bd467df0bbcf9e669662012
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2019-04-07 17:16:51 +0200

    désactivation csrf

--- src/main/java/com/terrier/finances/gestion/services/communs/api/config/security/SecurityTokenConfig.java
+++ src/main/java/com/terrier/finances/gestion/services/communs/api/config/security/SecurityTokenConfig.java
@@ -39,6 +39,7 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.csrf().disable()
 		// make sure we use stateless session; session won't be used to store user's state.
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 	
 		.and()
