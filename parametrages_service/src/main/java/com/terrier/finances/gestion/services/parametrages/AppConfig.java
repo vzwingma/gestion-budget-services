f81ec75c0f486368a6fc7ac88c8fbc8435224b5f
Vincent ZWINGMANN <vincent.zwingmann@gmail.com>
2019-09-04 15:10:06 +0200

    Suppression du scheduling

--- parametrages_service/src/main/java/com/terrier/finances/gestion/services/parametrages/AppConfig.java
+++ parametrages_service/src/main/java/com/terrier/finances/gestion/services/parametrages/AppConfig.java
@@ -4,16 +4,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableScheduling
 @EnableAsync
+@EnableWebMvc
 @ComponentScan(basePackages = { 
+		"com.terrier.finances.gestion.services.parametrages.api",
+		"com.terrier.finances.gestion.services.parametrages.api.config.security",
 		"com.terrier.finances.gestion.services.parametrages.business",
 		"com.terrier.finances.gestion.services.parametrages.data",
 		"com.terrier.finances.gestion.services.communs.data.mongodb",
-		"com.terrier.finances.gestion.services.parametrages.api.config.security",
+		"com.terrier.finances.gestion.services.communs.api.config",	
 		"com.terrier.finances.gestion.services.communs.api.interceptors"
 		})
 @PropertySource(value={"classpath:config.properties"}, ignoreResourceNotFound = true)
