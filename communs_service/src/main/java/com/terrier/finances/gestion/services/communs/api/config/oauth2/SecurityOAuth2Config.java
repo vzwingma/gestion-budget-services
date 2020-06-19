07589a0f08fe0013101d985900d3da6658ba0ece
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2020-06-19 21:55:19 +0200

    v12.2

--- communs_service/src/main/java/com/terrier/finances/gestion/services/communs/api/config/oauth2/SecurityOAuth2Config.java
+++ communs_service/src/main/java/com/terrier/finances/gestion/services/communs/api/config/oauth2/SecurityOAuth2Config.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * Configuration des API. Autorisées par OpenID
