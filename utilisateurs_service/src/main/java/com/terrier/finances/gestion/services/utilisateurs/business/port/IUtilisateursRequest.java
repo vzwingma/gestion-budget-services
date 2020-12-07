7f2de742b37a3ff0cb27cd79869f3fca95a73439
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2020-12-07 12:06:49 +0100

    Correction erreur de BDD lors de l'auth

--- utilisateurs_service/src/main/java/com/terrier/finances/gestion/services/utilisateurs/business/port/IUtilisateursRequest.java
+++ utilisateurs_service/src/main/java/com/terrier/finances/gestion/services/utilisateurs/business/port/IUtilisateursRequest.java
@@ -1,5 +1,6 @@
 package com.terrier.finances.gestion.services.utilisateurs.business.port;
 
+import com.terrier.finances.gestion.communs.utils.exceptions.DataNotFoundException;
 import com.terrier.finances.gestion.services.utilisateurs.business.model.v12.Utilisateur;
 
 import java.time.LocalDateTime;
@@ -13,14 +14,16 @@ public interface IUtilisateursRequest {
      * Chargement d'un utilisateur
      * @param idUtilisateur login de l'utilisateur
      * @return Utilisateur correspondant au login
+     * @throws DataNotFoundException erreur d'accès
      */
-    public Utilisateur getUtilisateur(String idUtilisateur);
+    public Utilisateur getUtilisateur(String idUtilisateur) throws DataNotFoundException;
 
 
     /**
      * Date de dernier accès
      * @param idUtilisateur login de l'utilisateur
      * @return date de dernier accès
+     * @throws DataNotFoundException erreur d'accès
      */
-    public LocalDateTime getLastAccessDate(String idUtilisateur);
+    public LocalDateTime getLastAccessDate(String idUtilisateur) throws DataNotFoundException;
 }
