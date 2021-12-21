96586a7603e343f8258cc6ff02371159f4e3e1e2
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2021-12-21 10:25:48 +0100

    ajout du converter pour swagger

--- communs_service/src/main/java/com/terrier/finances/gestion/services/communs/api/config/RessourcesConfig.java
+++ communs_service/src/main/java/com/terrier/finances/gestion/services/communs/api/config/RessourcesConfig.java
@@ -1,21 +1,17 @@
 package com.terrier.finances.gestion.services.communs.api.config;
 
-import java.util.List;
-
+import com.terrier.finances.gestion.services.communs.api.converters.APIObjectMessageConverter;
+import com.terrier.finances.gestion.services.communs.api.interceptors.IncomingRequestInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
-import com.terrier.finances.gestion.services.communs.api.converters.APIObjectMessageConverter;
-import com.terrier.finances.gestion.services.communs.api.interceptors.IncomingRequestInterceptor;
+import java.util.List;
 
 
 @Configuration
@@ -39,6 +35,9 @@ public class RessourcesConfig implements WebMvcConfigurer{
 	 */
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+		messageConverter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.ALL));
+		converters.add(messageConverter);
 		converters.add(new APIObjectMessageConverter<>());
         converters.add(new MappingJackson2HttpMessageConverter());
 		WebMvcConfigurer.super.configureMessageConverters(converters);
@@ -52,7 +51,7 @@ public class RessourcesConfig implements WebMvcConfigurer{
 		registry.addInterceptor(requestInterceptor);
 	}
 
-
+/**
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html")
@@ -61,4 +60,5 @@ public class RessourcesConfig implements WebMvcConfigurer{
 		registry.addResourceHandler("/webjars/**")
 		.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+	**/
 }
