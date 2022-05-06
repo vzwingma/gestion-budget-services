f77441cd08062f7267c8e99802f695c702c506c7
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2022-05-06 15:41:11 +0200

    Ajout de la config openAPI

--- parametrages/src/main/java/io/github/vzwingma/finances/budget/services/parametrages/api/ParametragesResource.java
+++ parametrages/src/main/java/io/github/vzwingma/finances/budget/services/parametrages/api/ParametragesResource.java
@@ -2,6 +2,11 @@ package io.github.vzwingma.finances.budget.services.parametrages.api;
 
 import io.github.vzwingma.finances.budget.services.communs.data.parametrages.model.CategorieOperation;
 import io.github.vzwingma.finances.budget.services.communs.utils.data.BudgetApiUrlEnum;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.jboss.logging.Logger;
 
 import javax.ws.rs.GET;
@@ -14,9 +19,7 @@ import java.util.List;
 @Path(BudgetApiUrlEnum.PARAMS_BASE)
 public class ParametragesResource {
 
-
     private static final Logger LOG = Logger.getLogger(ParametragesResource.class);
-
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -25,21 +28,19 @@ public class ParametragesResource {
     }
 
 
-
     /**
      * @return la liste des catégories d'opérations
-     * @throws DataNotFoundException erreur données non trouvées
-    @Operation(method = "GET", summary = "Liste des catégories d'opérations", tags={"Paramétrages"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Opération réussie",
+     **/
+     // @throws DataNotFoundException erreur données non trouvées
+    @Operation(description = "Liste les catégories d'opérations", summary = "Liste des catégories d'opérations")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Opération réussie",
                     content = { @Content(mediaType = "application/json",
                             schema = @Schema(implementation = CategorieOperation.class)) }),
-            @ApiResponse(responseCode = "401", description = "L'action n'est pas authentifiée"),
-            @ApiResponse(responseCode = "403", description = "L'opération n'est pas autorisée"),
-            @ApiResponse(responseCode = "404", description = "Session introuvable")
+            @APIResponse(responseCode = "401", description = "L'action n'est pas authentifiée"),
+            @APIResponse(responseCode = "403", description = "L'opération n'est pas autorisée"),
+            @APIResponse(responseCode = "404", description = "Session introuvable")
     })
-
-*/
     @GET
     @Path(BudgetApiUrlEnum.PARAMS_CATEGORIES)
     @Produces(MediaType.APPLICATION_JSON)
