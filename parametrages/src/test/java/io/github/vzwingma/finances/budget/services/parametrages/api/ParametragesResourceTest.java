1bb7b0e913831a0ff59c8afcc6a8fd3481e7846d
vzwingma <vincent.zwingmann@gmail.com>
2022-05-13 16:13:52 +0200

    TU sur le service Parametrages

--- parametrages/src/test/java/io/github/vzwingma/finances/budget/services/parametrages/api/ParametragesResourceTest.java
+++ parametrages/src/test/java/io/github/vzwingma/finances/budget/services/parametrages/api/ParametragesResourceTest.java
@@ -1,8 +1,14 @@
 package io.github.vzwingma.finances.budget.services.parametrages.api;
 
 import io.github.vzwingma.finances.budget.services.communs.utils.data.BudgetApiUrlEnum;
+import io.github.vzwingma.finances.budget.services.parametrages.business.ports.IParametrageAppProvider;
+import io.github.vzwingma.finances.budget.services.parametrages.test.MockDataCategoriesOperations;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.smallrye.mutiny.Uni;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -19,4 +25,16 @@ class ParametragesResourceTest {
              .body(is("API Parametrages"));
     }
 
+    @InjectMock
+    IParametrageAppProvider parametragesService;
+    @Test
+    void testGetCategories() {
+        // Init des données
+        Mockito.when(parametragesService.getCategories()).thenReturn(Uni.createFrom().item(MockDataCategoriesOperations.getListeTestCategories()));
+        // Test
+        given() .when().get(BudgetApiUrlEnum.PARAMS_BASE + BudgetApiUrlEnum.PARAMS_CATEGORIES)
+                .then()
+                .statusCode(200)
+                .body(Matchers.containsString(MockDataCategoriesOperations.getListeTestCategories().get(0).getLibelle()));
+    }
 }
