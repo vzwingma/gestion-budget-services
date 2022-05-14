d23ee01074f1e203c8684654577b42416836f2b2
vzwingma <vincent.zwingmann@gmail.com>
2022-05-14 14:32:00 +0200

    Suppression des TI natifs

--- parametrages/src/test/java/io/github/vzwingma/finances/budget/services/parametrages/api/ParametrageResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.github.vzwingma.finances.budget.services.parametrages.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ParametrageResourceIT extends ParametragesResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- utilisateurs/src/test/java/io/github/vzwingma/finances/budget/services/utilisateurs/api/UtilisateursResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.github.vzwingma.finances.budget.services.utilisateurs.api;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class UtilisateursResourceIT extends UtilisateursResourceTest {
-
-    // Execute the same tests but in native mode.
-}
